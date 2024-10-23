04f5a7eec213455cdfeb6e41b23f4d8936ce17e5
victor bratov <victor@bratov>
2024-10-23 17:49:48 +0100

    initial commit

---
+++ nvim/lua/plugins/rose-pine.lua
@@ -0,0 +1,11 @@
+return {
+  { "rose-pine/neovim", name = "rose-pine" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  }
+}
